788a5666f4717f9b09dbf7ecf5b19e0a257e8f0e
Fernando Felício <fernando_felicio82@yahoo.com.br>
2018-03-11 23:47:50 -0300

    Aula 4

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,12 @@
+public class TestaVariaveis {
+	public static void main(String args[]) {
+		int idade;
+		idade = 35;
+
+		System.out.println("Minha idade é: " + idade + " anos");
+
+		idade = 36;
+
+		System.out.println("Irei fazer : " + idade + " anos");
+	}
+}
